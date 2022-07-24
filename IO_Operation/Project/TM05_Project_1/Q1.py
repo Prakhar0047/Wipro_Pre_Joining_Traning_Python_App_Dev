af3a8cbc5ca14aa74a8bdf3f9df84e2ce5d4428b
Prakhar <prakharv047@gmail.com>
2022-07-25 00:31:52 +0530

    IO Project Fix

--- IO_Operation/Project/TM05_Project_1/Q1.py
+++ IO_Operation/Project/TM05_Project_1/Q1.py
@@ -1,26 +1,25 @@
-def secret(filename):
-  word_count = {}
-  with open(filename, 'r+') as infile:
-    ff = infile.read()
-    no_lines = len(ff)
-    # print("No of lines are: ", no_lines)
-    unique_words = set(ff.split())
-    words = ff.split()
-    # print("Unique Words are as follows: ", unique_words)
-    # print("Words are as follows: ", words)
-  
-  print("No of line",no_lines)
-  print("Unique Words",unique_words)
-  print("Words are:",words)
-
-  for x in unique_words:
-    word_count[words.count(x)] = x
-  
-  if no_lines>12:
-    print("Meeting Time:",24-no_lines,"PM")
-  else:
-    print("Meeting Time:",no_lines,"AM")
-
-  print("Meeting Place",word_count[max(word_count)],"Street")
-
-secret('sample.txt')
+fname = input("Enter file name: ")
+num_lines = 0
+frequency = 0
+frequent_word = ""
+words = []
+with open(fname, 'r') as f:
+    for line in f:
+        num_lines += 1
+        line_word = line.lower().replace(',','').replace('.','').split(" ")
+        for w in line_word:  
+            words.append(w)
+    for i in range(0, len(words)):
+        count = 1
+        for j in range(i+1, len(words)):  
+            if(words[i] == words[j]):  
+                count = count + 1
+        if(count > frequency):  
+            frequency = count 
+            frequent_word = words[i]
+if num_lines > 12:
+    num_lines = num_lines - 12
+    print("Metting time:",num_lines,"P.M")
+else:
+    print("Metting time:",num_lines,"A.M")
+print("Meting Place:",frequent_word.capitalize(),"Street")
